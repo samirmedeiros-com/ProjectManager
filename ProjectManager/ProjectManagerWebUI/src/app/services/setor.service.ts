45dbfcc352fcbc2c9fd2972a8b7ecdac1ccb632c
samirmedeiros-com <samir@samirmedeiros.com>
2026-06-08 07:54:54 +0100

    Import environment from environments folder

--- ProjectManager/ProjectManagerWebUI/src/app/services/setor.service.ts
+++ ProjectManager/ProjectManagerWebUI/src/app/services/setor.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Setor } from '../models/setor.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
